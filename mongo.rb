c841d879137262d4640bd4074790060fb3c2ed7b
potapovDim <dmytrop@weblium.com>
2017-04-14 14:44:29 +0300

    training

--- mongo.rb
+++ mongo.rb
@@ -1,5 +1,11 @@
 require 'mongo'
 
-client = Mongo::Client.new([ '127.0.0.1:27050' ])
+client = Mongo::Client.new([ '127.0.0.1:27050' ],
+        :database => 'test_new_db')
+collection = client[:people]
 
-puts client.list_databases
+doc = { name: 'Steve', hobbies: [ 'hiking', 'tennis', 'fly fishing' ] }
+
+result = collection.insert_one(doc) #insert_one - додати один документ об'єкт> insert_many -> array
+
+result.n # returns 1, because one document was inserted
