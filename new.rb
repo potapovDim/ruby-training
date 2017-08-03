221b24a9bb8ec3cee124fac01a0a73b6ab10dff8
potapovDim <potapovd3012@gmail.com>
2017-08-03 10:45:09 +0300

    training

--- new.rb
+++ new.rb
@@ -1,1 +1,6 @@
-puts 'test'.include? 't'
+a = [*1,2,3]
+puts a
+puts '---------------'
+b = '1'
+a << b
+puts a
--- tr.rb
+++ tr.rb
@@ -1,25 +1,61 @@
-# require 'yaml'
+#!/usr/bin/env ruby -I ../lib -I lib
+# coding: utf-8
+require 'sinatra'
 
-# CONFIG = YAML.load(File.read(File.join(File.dirname(__FILE__), "./tr.yml")))
+set :server, 'thin'
+connections = []
 
-# string_builder = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+get '/' do
+  halt erb(:login) unless params[:user]
+  erb :chat, :locals => { :user => params[:user].gsub(/\W/, '') }
+end
 
-# initial_prefix_email = ''
+get '/stream', :provides => 'text/event-stream' do
+  stream :keep_open do |out|
+    connections << out
+    out.callback { connections.delete(out) }
+  end
+end
 
-# for i in 0..10 do
-#   puts i, string_builder[rand(0..62)]
-#   initial_prefix_email += string_builder[rand(0..62)]
-# end
+post '/' do
+  connections.each { |out| out << "data: #{params[:msg]}\n\n" }
+  204 # response without entity body
+end
 
-# puts '' + 'b', initial_prefix_email
+__END__
 
-# a = {
-#   d:"10",
-#   c: 100
-# }
+@@ layout
+<html>
+  <head>
+    <title>Super Simple Chat with Sinatra</title>
+    <meta charset="utf-8" />
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+  </head>
+  <body><%= yield %></body>
+</html>
 
-# puts a[:c]
+@@ login
+<form action='/'>
+  <label for='user'>User Name:</label>
+  <input name='user' value='' />
+  <input type='submit' value="GO!" />
+</form>
 
-arr = Array.new 255
-new = arr.join '2'
-puts new.length
+@@ chat
+<pre id='chat'></pre>
+<form>
+  <input id='msg' placeholder='type message here...' />
+</form>
+
+<script>
+  // reading
+  var es = new EventSource('/stream');
+  es.onmessage = function(e) { $('#chat').append(e.data + "\n") };
+
+  // writing
+  $("form").on('submit',function(e) {
+    $.post('/', {msg: "<%= user %>: " + $('#msg').val()});
+    $('#msg').val(''); $('#msg').focus();
+    e.preventDefault();
+  });
+</script>
